031ed27f2c2269dcdb610992aef1dc88fe0bd9ba
Malik Mustafa <attaul662@email.com>
2026-05-20 09:14:41 +0500

    Add trapping rain water solution

---
+++ trapping_rain_water.py
@@ -0,0 +1,22 @@
+height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+
+
+def trap(height):
+    l, r = 0, len(height) - 1
+    left_max, right_max = height[l], height[r]
+    water = 0
+
+    while l < r:
+        if left_max < right_max:
+            l += 1
+            left_max = max(left_max, height[l])
+            water += left_max - height[l]
+        else:
+            r -= 1
+            right_max = max(right_max, height[r])
+            water += right_max - height[r]
+
+    return water
+
+
+print(trap(height))
